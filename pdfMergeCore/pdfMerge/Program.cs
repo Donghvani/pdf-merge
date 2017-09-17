7c5303f0256b0b7caaa6a2688709391304c5b23b
Vitali Donghvani <vitali.donghvani@live.com>
2017-09-17 22:35:49 +0100

    pdf merge

---
+++ pdfMergeCore/pdfMerge/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+
+namespace pdfMerge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Started pdf merger");
+            string pdfFileLocation = @"C:\Temp\bookpages\";
+            int numberOfPdfs = 671;           
+
+            List<string> pdfs = new List<string>();
+            for (int docIndex = 0; docIndex < numberOfPdfs + 1; docIndex++)
+            {
+                string fileName = $"{docIndex}.pdf";
+                pdfs.Add(pdfFileLocation + fileName);
+            }
+            CombineMultiplePdFs(pdfs, @"C:\Temp\bookpages\merged\merge.pdf");
+        }
+
+        public static void CombineMultiplePdFs(List<string> fileNames, string outFile)
+        {
+            // step 1: creation of a document-object
+            Document document = new Document();
+
+            // step 2: we create a writer that listens to the document
+            PdfCopy writer = new PdfCopy(document, new FileStream(outFile, FileMode.Create));
+
+            // step 3: we open the document
+            document.Open();
+
+            foreach (string fileName in fileNames)
+            {
+                // we create a reader for a certain document
+                PdfReader reader = new PdfReader(fileName);
+                reader.ConsolidateNamedDestinations();
+
+                // step 4: we add content
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    PdfImportedPage page = writer.GetImportedPage(reader, i);
+                    writer.AddPage(page);
+                }
+
+                PRAcroForm form = reader.AcroForm;
+                if (form != null)
+                {
+                    writer.CopyDocumentFields(reader);
+                }
+
+                reader.Close();
+                Console.WriteLine("added file: " + fileName);
+            }
+
+            // step 5: we close the document and writer
+            writer.Close();
+            document.Close();
+        }
+    }
+}
